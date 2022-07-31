4d9c934d08552ad691e08f0d8013cc75d1bb98fd
orchsik <orchsik@naver.com>
2022-07-31 17:26:32 +0900

    Add 03-tonumber

--- __test__/ts/03-toNumber.spec.ts
+++ __test__/ts/03-toNumber.spec.ts
@@ -1,61 +1,34 @@
-import {
-  solution,
-  separateByNumber,
-  charToNum,
-  separteNumberCalling,
-} from "../../src/ts/03-toNumber";
-
-// describe("input string convert to Number", () => {
-//   it("#1 문자열을 숫자기준으로 끊어서 배열에 담아준다.", () => {
-//     const answer = separateByNumber("one4seveneight");
-//     expect(answer).toEqual(["one", "4", "seveneight"]);
-//   });
-
-//   it("#2 주어진 배열의 아이템을 숫자로 변환(one -> 1)", () => {
-//     const answer = charToNum(["one", "4", "seveneight"]);
-//     expect(answer).toEqual(["1", "4", "seveneight"]);
-//   });
-
-//   it("#3 문자열을 숫자영문명칭(ex. one, two)으로 분리하기", () => {
-//     const answer = separteNumberCalling("seveneight");
-//     expect(answer).toEqual(["seven", "eight"]);
-//   });
-
-//   it("convert finally", () => {
-//     const answer = solution("one4seveneight");
-//     expect(answer).toBe(1478);
-//   });
-// });
+import { solution } from "../../src/ts/03-toNumber";
 
 describe("various solution case", () => {
-  // it("one", () => {
-  //   const answer = solution("one");
-  //   expect(answer).toBe(1);
-  // });
-  // it("one1", () => {
-  //   const answer = solution("one1");
-  //   expect(answer).toBe(11);
-  // });
-  // it("one4seveneight", () => {
-  //   const answer = solution("one4seveneight");
-  //   expect(answer).toBe(1478);
-  // });
-  // it("one4seveneight9", () => {
-  //   const answer = solution("one4seveneight9");
-  //   expect(answer).toBe(14789);
-  // });
-  // it("23four5six7", () => {
-  //   const answer = solution("23four5six7");
-  //   expect(answer).toBe(234567);
-  // });
-  // it("2three45sixseven", () => {
-  //   const answer = solution("2three45sixseven");
-  //   expect(answer).toBe(234567);
-  // });
-  // it("123", () => {
-  //   const answer = solution("123");
-  //   expect(answer).toBe(123);
-  // });
+  it("one", () => {
+    const answer = solution("one");
+    expect(answer).toBe(1);
+  });
+  it("one1", () => {
+    const answer = solution("one1");
+    expect(answer).toBe(11);
+  });
+  it("one4seveneight", () => {
+    const answer = solution("one4seveneight");
+    expect(answer).toBe(1478);
+  });
+  it("one4seveneight9", () => {
+    const answer = solution("one4seveneight9");
+    expect(answer).toBe(14789);
+  });
+  it("23four5six7", () => {
+    const answer = solution("23four5six7");
+    expect(answer).toBe(234567);
+  });
+  it("2three45sixseven", () => {
+    const answer = solution("2three45sixseven");
+    expect(answer).toBe(234567);
+  });
+  it("123", () => {
+    const answer = solution("123");
+    expect(answer).toBe(123);
+  });
   it("123onetwo", () => {
     const answer = solution("123onetwo");
     expect(answer).toBe(12312);
--- src/ts/03-toNumber.ts
+++ src/ts/03-toNumber.ts
@@ -1,15 +1,24 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/81301
-// [TODO]
+// [O]
 
 const solution = (input: string) => {
-  const result = [];
-  charToNum(separateByNumber(input)).forEach((str) => {
-    result.push(...separteNumberCalling(str));
-  });
-  return Number(charToNum(result).join(""));
+  let result = "";
+  let word = "";
+  for (let char of input.split("")) {
+    if (char.match(/\d/)) {
+      result += char;
+    } else {
+      word += char;
+      if (DICT[word]) {
+        result += DICT[word];
+        word = "";
+      }
+    }
+  }
+  return Number(result);
 };
 
-const PIVOT = {
+const DICT = {
   zero: "0",
   one: "1",
   two: "2",
@@ -22,44 +31,4 @@ const PIVOT = {
   nine: "9",
 };
 
-const charToNum = (input: string[]) => input.map((item) => PIVOT[item] || item);
-
-function separateByNumber(input: string) {
-  const numberMatch = (input) => input.match(/\d/);
-
-  const sliceByMatch = ({ input, index }) => ({
-    word: input.slice(0, index),
-    number: input[index],
-    others: input.slice(index + 1),
-  });
-
-  let source = input;
-  const separated = [];
-  while (numberMatch(source)) {
-    const sliced = sliceByMatch(numberMatch(source));
-    source = sliced.others;
-    separated.push(sliced.word, sliced.number);
-    if (!numberMatch(sliced.others)) {
-      separated.push(sliced.others);
-    }
-  }
-
-  if (separated.length === 0) separated.push(source);
-  return separated;
-}
-
-const separteNumberCalling = (input: string) => {
-  const result = [];
-  let characters = [];
-  input.split("").forEach((char, i) => {
-    characters.push(char);
-    const str = characters.join("");
-    if (Number(PIVOT[str] || str)) {
-      characters = [];
-      result.push(str);
-    }
-  });
-  return result;
-};
-
-export { solution, separateByNumber, charToNum, separteNumberCalling };
+export { solution };
